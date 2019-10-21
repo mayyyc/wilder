5089df5f4e4ad76b02276de2a686f66bbba06efc
May Chen <may@Mays-MacBook-Pro.local>
2019-10-21 23:06:01 +1100

    image working

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,30 @@ if ("geolocation" in navigator) {
 } else {
   console.log("geolocation is not available ");
 }
-const pageSize = 3;
+const pageSize = 10;
 const App: React.FC = () => {
   const [animals, setAnimals] = useState(undefined);
   const [viewedAnimals, setViewedAnimals] = useState(0);
+  const [isFetching, setIsFetching] = useState(false);
   const getAnimals = () => {
+    setIsFetching(true);
     navigator.geolocation.getCurrentPosition(position => {
       axios
         .get(
           `https://biocache-ws.ala.org.au/ws/occurrences/search.json?q=*%3A*&fq=class%3A%22Mammalia%22&fq=family%3A%22Macropodidae%22&lat=${position.coords.latitude}&lon=${position.coords.longitude}&radius=100&fq=multimedia:Image&facet=false&pageSize=${pageSize}&start=${viewedAnimals}&sort=identification_qualifier_s&dir=asc`
         )
         .then(res => {
+          setIsFetching(false);
           setAnimals(res.data.occurrences);
-          console.log("res.data.occurrences :", res.data.occurrences);
+          setViewedAnimals(prev => prev + pageSize);
+          // console.log("res.data.occurrences :", res.data.occurrences);
         });
     });
-    setViewedAnimals(prev => prev + pageSize);
   };
   useEffect(() => {
     getAnimals();
   }, []);
-  if (!animals) return <div>preparing to get you wilder</div>;
+  if (!animals || isFetching) return <div>preparing to get you wilder</div>;
   if (isEmpty(animals)) return <div>nothing is wilder than you are</div>;
   return <Cards animals={animals} onFinished={getAnimals} />;
 };
--- src/Cards.tsx
+++ src/Cards.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import drop from "lodash/drop";
 import isEmpty from "lodash/isEmpty";
 import { Card } from "./Card";
 export const Cards = ({ animals, onFinished }) => {
   const [cards, setCards] = useState(animals);
+  useEffect(() => {
+    setCards(animals);
+  }, [animals]);
   const handleSwipeCard = () => {
-    if (isEmpty(cards)) onFinished();
     setCards(drop(cards));
+    if (cards.length === 1) {
+      onFinished();
+    }
   };
-  if (isEmpty(cards)) return <div></div>;
+  if (isEmpty(cards)) {
+    return <div></div>;
+  }
   return (
     <div onClick={handleSwipeCard}>
       <Card card={cards[0]} />
