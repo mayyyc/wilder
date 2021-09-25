ca96e794d4a0fcda0ecd5ad00ac53745b2bd57da
May Chen <may@Mays-MacBook-Pro.local>
2021-09-25 12:11:57 +1000

    github page

---
+++ docs/precache-manifest.534034db0b0dff53c84d9c00491065d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed29fdac48fc92182454880beca8844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79f872eb2ef033168670",
+    "url": "/static/css/main.456f1e6b.chunk.css"
+  },
+  {
+    "revision": "fe2cfec9624ec12e158e",
+    "url": "/static/js/2.25ab41b9.chunk.js"
+  },
+  {
+    "revision": "79f872eb2ef033168670",
+    "url": "/static/js/main.eecf2309.chunk.js"
+  },
+  {
+    "revision": "e568c113f37ee66623f9",
+    "url": "/static/js/runtime-main.e918d391.js"
+  }
+]);
